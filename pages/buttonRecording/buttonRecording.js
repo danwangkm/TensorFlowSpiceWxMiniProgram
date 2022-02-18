3c413671e82fd0f7947a81a578f2f8b70bd586bf
danwangkm <danwangkm@gmail.com>
2022-02-18 10:46:40 -0800

    Decode data in OnFrame callback. Test in IOS

--- pages/buttonRecording/buttonRecording.js
+++ pages/buttonRecording/buttonRecording.js
@@ -22,51 +22,54 @@ Page({
       this.tempFilePath = res.tempFilePath;
       console.log('停止录音', res.tempFilePath);
     });
+    let prefix;
+    let finalAudio;
     recorderManager.onFrameRecorded((res) => {
       // recordList.push(res.frameBuffer);
       console.log(`bytelength: ${res.frameBuffer.byteLength}`);
+      let arrayBuffer = res.frameBuffer;
+      let temp = arrayBuffer.slice(0);
+      if (prefix === undefined) {
+        prefix = arrayBuffer.slice(0, 4);
+      } else {
+        temp = this.appendBuffer(prefix, arrayBuffer);
+      }
+      audioCtx.decodeAudioData(temp, (res) => {
+        console.log(`yes: ${res.getChannelData(0)}`);
+        // if (!finalAudio) {
+        //   finalAudio = res;
+        // } else {
+        //   let tmp = audioCtx.createBuffer(1, (finalAudio.length + res.length), finalAudio.sampleRate );
+        //   let channel = tmp.getChannelData(0);
+        //   channel.set( finalAudio.getChannelData(0), 0);
+        //   channel.set( res.getChannelData(0), finalAudio.length);
+        //   finalAudio = tmp;
+        // }
+        // if (index === recordList.length - 1) {
+        //   console.log('final index');
+        //   source.buffer = finalAudio;
+        //   source.connect(audioCtx.destination);
+        //   source.start();
+        // }
+      }, (error) => {
+        console.error(error);
+      });
     });
+  },
+  appendBuffer(buffer1, buffer2) {
+    var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
+    tmp.set(new Uint8Array(buffer1), 0);
+    tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
+    return tmp.buffer;
   },
   onPlayFlamesHandler(e) {
     console.log(`start size of buffer: ${recordList.length}`);
     let source = audioCtx.createBufferSource();
     
-    var _appendBuffer = function(buffer1, buffer2) {
-      var tmp = new Uint8Array(buffer1.byteLength + buffer2.byteLength);
-      tmp.set(new Uint8Array(buffer1), 0);
-      tmp.set(new Uint8Array(buffer2), buffer1.byteLength);
-      return tmp.buffer;
-    };
-    let prefix;
-    let finalAudio;
+    
+    
     for (let index = 0; index<recordList.length; index++) {
-      let arrayBuffer = recordList[index];
-      let temp = arrayBuffer.slice(0);
-      if (index === 0) {
-        prefix = arrayBuffer.slice(0, 4);
-      } else {
-        temp = _appendBuffer(prefix, arrayBuffer);
-      }
-      audioCtx.decodeAudioData(temp, (res) => {
-        console.log(`yes ${index}: ${res.getChannelData(0)}`);
-        if (!finalAudio) {
-          finalAudio = res;
-        } else {
-          let tmp = audioCtx.createBuffer(1, (finalAudio.length + res.length), finalAudio.sampleRate );
-          let channel = tmp.getChannelData(0);
-          channel.set( finalAudio.getChannelData(0), 0);
-          channel.set( res.getChannelData(0), finalAudio.length);
-          finalAudio = tmp;
-        }
-        if (index === recordList.length - 1) {
-          console.log('final index');
-          source.buffer = finalAudio;
-          source.connect(audioCtx.destination);
-          source.start();
-        }
-      }, (error) => {
-        console.error(error);
-      });
+      
     }
   },
   onStartTapHandler(event) {
